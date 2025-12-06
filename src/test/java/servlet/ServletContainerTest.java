dfbfdb6e406ce13fac7e1ea8b8aa38b98dbe5ccf
wocks1123 <wocks1123@gmail.com>
2025-12-07 00:12:51 +0900

    test: ServletContainer의 와일드카드 패턴 테스트 메서드 구현
    - 기존 ServletContainer::findServlet대신 resolveServlet을 사용하도록 유도

--- src/test/java/servlet/ServletContainerTest.java
+++ src/test/java/servlet/ServletContainerTest.java
@@ -17,16 +17,11 @@ class ServletContainerTest {
         // given
         final ServletContainer servletContainer = new ServletContainer();
         final String path = "/test";
-        final Servlet servlet = new Servlet() {
-            @Override
-            public void service(HttpRequest request, HttpResponse response) {
-                // do nothing
-            }
-        };
+        final Servlet servlet = new DummyServlet();
 
         // when
         servletContainer.registerServlet(path, servlet);
-        final Servlet foundServlet = servletContainer.findServlet(path);
+        final Servlet foundServlet = servletContainer.resolveServlet(path);
 
         // then
         assertNotNull(foundServlet);
@@ -41,7 +36,7 @@ class ServletContainerTest {
         final String path = "/not-registered";
 
         // when
-        final Servlet foundServlet = servletContainer.findServlet(path);
+        final Servlet foundServlet = servletContainer.resolveServlet(path);
 
         // then
         assertNull(foundServlet);
@@ -51,40 +46,77 @@ class ServletContainerTest {
     @DisplayName("와일드카드 패턴으로 서블릿을 등록할 수 있다")
     void testRegisterServletWithWildcardPattern() {
         // given
+        final ServletContainer servletContainer = new ServletContainer();
+        final String wildcardPath = "/api/*";
+        final Servlet servlet = new DummyServlet();
 
         // when
+        servletContainer.registerServlet(wildcardPath, servlet);
+        final Servlet foundServlet = servletContainer.resolveServlet("/api/test");
 
         // then
+        assertNotNull(foundServlet);
+        assertEquals(servlet, foundServlet);
     }
 
     @Test
     @DisplayName("와일드카드 패턴과 일치하지 않는 경로는 매칭되지 않는다")
     void testWildcardPatternDoesNotMatchDifferentPath() {
         // given
+        final ServletContainer servletContainer = new ServletContainer();
+        final String wildcardPath = "/api/*";
+        final Servlet servlet = new DummyServlet();
 
         // when
+        servletContainer.registerServlet(wildcardPath, servlet);
 
         // then
+        assertNull(servletContainer.resolveServlet("/static/test"));
+        assertNull(servletContainer.resolveServlet("/static"));
+        assertNull(servletContainer.resolveServlet("/api"));
     }
 
     @Test
     @DisplayName("정확한 경로 매칭이 와일드카드 패턴보다 우선한다")
     void testExactPathMatchHasPriorityOverWildcard() {
         // given
+        final ServletContainer servletContainer = new ServletContainer();
+        final String wildcardPath = "/api/*";
+        final String specificPath = "/api/test";
+        final Servlet wildcardServlet = new DummyServlet();
+        final Servlet specificServlet = new DummyServlet();
 
         // when
+        servletContainer.registerServlet(wildcardPath, wildcardServlet);
+        servletContainer.registerServlet(specificPath, specificServlet);
+        final Servlet foundServlet = servletContainer.resolveServlet(specificPath);
 
         // then
+        assertEquals(specificServlet, foundServlet);
     }
 
     @Test
     @DisplayName("루트 와일드카드 패턴으로 모든 경로를 처리할 수 있다")
     void testRootWildcardPatternMatchesAllPaths() {
         // given
+        final ServletContainer servletContainer = new ServletContainer();
+        final String rootWildcardPath = "/*";
+        final Servlet servlet = new DummyServlet();
 
         // when
+        servletContainer.registerServlet(rootWildcardPath, servlet);
 
         // then
+        assertEquals(servlet, servletContainer.resolveServlet("/any/path"));
+        assertEquals(servlet, servletContainer.resolveServlet("/another/path"));
+        assertEquals(servlet, servletContainer.resolveServlet("/"));
+    }
+
+    private static class DummyServlet implements Servlet {
+        @Override
+        public void service(HttpRequest request, HttpResponse response) {
+            // do nothing
+        }
     }
 
 }
