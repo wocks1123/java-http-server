f3bd74fdbe4c27d81a27dddcb1d9ab363f64b12c
wocks1123 <wocks1123@gmail.com>
2025-12-06 17:43:13 +0900

    test: RequestHandler에 대한 테스트 케이스 작성

---
+++ src/test/java/server/RequestHandlerTest.java
@@ -0,0 +1,29 @@
+package server;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class RequestHandlerTest {
+
+    private RequestHandler sut;
+
+
+    @Test
+    @DisplayName("InputStream의 HTTP 텍스트가 HttpRequest로 파싱된다")
+    void handleParsesHttpRequest() {
+
+    }
+
+    @Test
+    @DisplayName("HttpHandler가 처리한 HttpResponse가 OutputStream으로 작성된다")
+    void handleWritesHttpResponseToOutputStream() {
+
+    }
+
+    @Test
+    @DisplayName("HttpHandler가 HttpRequest와 HttpResponse를 처리한다")
+    void handleInvokesHttpHandler() {
+
+    }
+
+}
