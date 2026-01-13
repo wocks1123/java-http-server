6d30e98dd936a48e44a1ca958c805fa8cb956dac
wocks1123 <wocks1123@gmail.com>
2026-01-13 21:51:31 +0900

    feat: TodoController 추가, FrontControllerServlet 적용

--- src/main/java/dev/labs/httpserver/HttpServerApplication.java
+++ src/main/java/dev/labs/httpserver/HttpServerApplication.java
@@ -1,10 +1,13 @@
 package dev.labs.httpserver;
 
 import dev.labs.httpserver.app.todo.InMemoryTodoRepository;
-import dev.labs.httpserver.app.todo.TodoServlet;
+import dev.labs.httpserver.app.todo.TodoController;
+import dev.labs.httpserver.app.todo.TodoRepository;
+import dev.labs.httpserver.controller.FrontControllerServlet;
+import dev.labs.httpserver.controller.Router;
+import dev.labs.httpserver.http.HttpMethod;
 import dev.labs.httpserver.http.HttpStatus;
 import dev.labs.httpserver.server.HttpServer;
-import dev.labs.httpserver.servlet.Servlet;
 import dev.labs.httpserver.servlet.ServletContainer;
 import dev.labs.httpserver.servlet.StaticResourceServlet;
 
@@ -13,16 +16,20 @@ public class HttpServerApplication {
     private static final int PORT = 8080;
 
     public static void main(String[] args) {
+        Router router = new Router();
+        TodoRepository todoRepository = new InMemoryTodoRepository();
+        TodoController todoController = new TodoController(todoRepository);
+        router.addRoute(HttpMethod.GET, "/todos", todoController::list);
+        router.addRoute(HttpMethod.POST, "/todos", todoController::create);
+        router.addRoute(HttpMethod.GET, "/todos/{id}", todoController::detail);
+
         ServletContainer servletContainer = new ServletContainer();
         servletContainer.registerServlet("/*", (request, response) -> {
             response.setStatus(HttpStatus.OK);
             response.setBody("Hello, World!\r\n");
         });
         servletContainer.registerServlet("/static/*", new StaticResourceServlet());
-
-        Servlet todoServlet = new TodoServlet(new InMemoryTodoRepository());
-        servletContainer.registerServlet("/todos/*", todoServlet);
-        servletContainer.registerServlet("/todos", todoServlet);
+        servletContainer.registerServlet("/*", new FrontControllerServlet(router));
 
         HttpServer server = new HttpServer(PORT, servletContainer);
         server.start();
---
+++ src/main/java/dev/labs/httpserver/app/todo/TodoController.java
@@ -0,0 +1,102 @@
+package dev.labs.httpserver.app.todo;
+
+import dev.labs.httpserver.http.HttpRequest;
+import dev.labs.httpserver.http.HttpResponse;
+import dev.labs.httpserver.http.HttpStatus;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+import static java.net.URLDecoder.decode;
+
+public class TodoController {
+
+    private static final Logger log = LoggerFactory.getLogger(TodoController.class);
+
+    private final TodoRepository todoRepository;
+
+    public TodoController(TodoRepository todoRepository) {
+        this.todoRepository = todoRepository;
+    }
+
+
+    public void create(HttpRequest request, HttpResponse response) {
+        Map<String, String> params = parseFormBody(request.getBody());
+        String title = params.get("title");
+
+        if (title == null || title.isBlank()) {
+            response.setStatus(HttpStatus.BAD_REQUEST);
+            return;
+        }
+
+        todoRepository.save(new Todo(title));
+        response.setStatus(HttpStatus.OK);
+    }
+
+    public void list(HttpRequest request, HttpResponse response) {
+        List<Todo> todos = todoRepository.findAll();
+
+        response.setStatus(HttpStatus.OK);
+        response.addHeader("Content-Type", "application/json");
+        response.setBody(todos.toString()); // FIXME: 직렬화
+    }
+
+    public void detail(HttpRequest request, HttpResponse response) {
+        TodoId id = extractIdFromPath(request.getPath());
+
+        if (id == null) {
+            response.setStatus(HttpStatus.BAD_REQUEST);
+            return;
+        }
+
+        Optional<Todo> todo = todoRepository.findById(id);
+
+        if (todo.isEmpty()) {
+            response.setStatus(HttpStatus.NOT_FOUND);
+            return;
+        }
+
+        response.setStatus(HttpStatus.OK);
+        response.addHeader("Content-Type", "application/json");
+        response.setBody(todo.get().toString());
+    }
+
+    private Map<String, String> parseFormBody(String body) {
+        Map<String, String> params = new HashMap<>();
+        if (body == null || body.isBlank()) {
+            return params;
+        }
+
+        String[] pairs = body.split("&");
+        for (String pair : pairs) {
+            int idx = pair.indexOf('=');
+            if (idx == -1) continue;
+
+            String name = decode(pair.substring(0, idx), StandardCharsets.UTF_8);
+            String value = decode(pair.substring(idx + 1), StandardCharsets.UTF_8);
+
+            params.put(name, value);
+        }
+        return params;
+    }
+
+    private TodoId extractIdFromPath(String path) {
+        String[] parts = path.split("/");
+        if (parts.length != 3 || !"todos".equals(parts[1])) {
+            return null;
+        }
+
+        try {
+            Long value = Long.parseLong(parts[2]);
+            return new TodoId(value);
+        } catch (NumberFormatException e) {
+            return null;
+        }
+    }
+
+}
