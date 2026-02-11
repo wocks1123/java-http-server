5515e97a62072017ec841035966ae6ae8bda1b69
wocks1123 <wocks1123@gmail.com>
2026-02-11 22:04:33 +0900

    refactor: TodoRegisterService에서 트랜잭션 경계 로직 제거

--- src/main/java/dev/labs/httpserver/app/todo/TodoRegisterService.java
+++ src/main/java/dev/labs/httpserver/app/todo/TodoRegisterService.java
@@ -17,24 +17,11 @@ public class TodoRegisterService {
     }
 
     public TodoId registerTodo(RegisterTodoCommand cmd) {
-        try {
-            tx.begin();
+        return tx.execute(() -> {
             Todo savedTodo = todoRepository.save(cmd.toTodo());
             todoStatsRepository.increaseTotalCount(cmd.userId());
-            tx.commit();
             return savedTodo.getId();
-        } catch (Exception ex) {
-            try {
-                tx.rollback();
-            } catch (Exception ignored) {
-            }
-            throw new RuntimeException(ex);
-        } finally {
-            try {
-                tx.end();
-            } catch (Exception ignored) {
-            }
-        }
+        });
     }
 
 }
